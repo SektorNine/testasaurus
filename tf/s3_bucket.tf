90f5eb8cebb143cba479c48ab29ea28b89e8009f
Jon Udell <jon@jonudell.info>
2023-09-25 15:47:11 -0700

    Update s3_bucket.tf

--- tf/s3_bucket.tf
+++ tf/s3_bucket.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket_public_access_block" "public_access_bucket_1" {
   bucket = aws_s3_bucket.my_bucket_1.id
 
-  block_public_acls   = true
+  block_public_acls   = false
   block_public_policy = true
   ignore_public_acls = true
   restrict_public_buckets = true
