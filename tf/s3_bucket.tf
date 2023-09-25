84fca11f3d9938f9777fb842b792640374547bda
Jon Udell <jon@jonudell.info>
2023-09-25 13:54:21 -0700

    Create s3_bucket.tf

---
+++ tf/s3_bucket.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket_public_access_block" "public_access_bucket_1" {
+  bucket = aws_s3_bucket.my_bucket_1.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_s3_bucket" "my_bucket_1" {
+  bucket = "test-project-foo-bucket-01"
+}
